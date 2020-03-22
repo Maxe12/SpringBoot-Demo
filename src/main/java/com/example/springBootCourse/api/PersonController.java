e00f6acbfc1464e2c4967af89576d9bc3c591fc5
Maxe <maxbieleke@web.de>
2020-03-22 17:34:23 +0100

    finished first endpoint for adding person to a fakeDB

--- src/main/java/com/example/springBootCourse/api/PersonController.java
+++ src/main/java/com/example/springBootCourse/api/PersonController.java
@@ -4,8 +4,11 @@ import com.example.springBootCourse.model.Person;
 import com.example.springBootCourse.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("api/v1/person")
 @RestController
 public class PersonController {
     private final PersonService personService;
@@ -16,7 +19,7 @@ public class PersonController {
     }
 
     @PostMapping
-    public void addPerson(Person person) {
+    public void addPerson(@RequestBody Person person) {
         this.personService.addPerson(person);
     }
 }
