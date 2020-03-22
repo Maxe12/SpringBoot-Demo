838b45c78bbe81cea5557bc7183bbed241a15f74
Maxe <maxbieleke@web.de>
2020-03-22 16:41:46 +0100

    initial commit

---
+++ src/main/java/com/example/springBootCourse/SpringBootCrashCourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springBootCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrashCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrashCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springBootCourse/SpringBootCrashCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springBootCourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrashCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
